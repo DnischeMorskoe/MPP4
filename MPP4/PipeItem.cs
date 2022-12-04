b42a3ae1e219165d0f31574b4f1ca492fb0097cc
dnisc <dnisc@DNISCHE-MORSKOE>
2022-12-04 17:13:32 +0300

    reading, generating and writing tasks

---
+++ MPP4/PipeItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPP4
+{
+    public class PipeItem
+    {
+        public string path;
+        public string code;
+
+        public PipeItem(string path)
+        {
+            this.path = path;
+        }
+
+        public PipeItem(string path, string code)
+        {
+            this.path = path;
+            this.code = code;
+        }
+
+    }
+}
--- MPP4/Program.cs
+++ MPP4/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Threading.Tasks.Dataflow;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -12,6 +14,28 @@ namespace MPP4
         {
             string path = @"D:\Учеба\3_курс\СПП\MPP4\MPP4\Files";
             string resultPath = @"D:\Учеба\3_курс\СПП\MPP4\ResultTests\";
+
+            List<string> pathes = new List<string>(Directory.GetFiles(path));
+            Pipeline pipeline = new Pipeline();
+            List<IEnumerable<PipeItem>> items = new List<IEnumerable<PipeItem>>();
+
+
+            for (int i = 0; i < pathes.Count; i += 2)
+            {
+                items.Add(pathes.GetRange(i, Math.Min(2, pathes.Count - i)).Select(x => new PipeItem(x)));
+            }
+
+            foreach (IEnumerable<PipeItem> item in items)
+            {
+                var read = new TransformBlock<PipeItem[], PipeItem[]>(Pipeline.Read);
+                var generate = new TransformBlock<PipeItem[], PipeItem[]>(Pipeline.Generate);
+                var write = new ActionBlock<PipeItem[]>(Pipeline.Write);
+
+                read.Post(item.ToArray());
+                read.Complete();
+
+            }
+
 
         }
     }
